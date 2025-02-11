5ed5d2ff1fd30a805506b273d822705a1683104a
Rain <rain@sunshowers.io>
2025-02-11 03:53:32 +0000

    add main.rs
    This makes it so that `cargo install nextest` produces an error message.

---
+++ crates/nextest/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    compile_error!(
+        r#"This is a stub crate for cargo-nextest. Install nextest with:
+
+    cargo install --locked cargo-nextest
+
+Or see https://get.nexte.st for pre-built binaries."#
+    );
+}
